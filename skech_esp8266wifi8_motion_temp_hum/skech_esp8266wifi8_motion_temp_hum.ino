869d35b0eaca059907eab95c396aeb2a7931b0a8
Nikola Milosevic <nikolamilosevic86@users.noreply.github.com>
2021-02-27 21:17:28 +0100

    Fix

--- skech_esp8266wifi8_motion_temp_hum/skech_esp8266wifi8_motion_temp_hum.ino
+++ skech_esp8266wifi8_motion_temp_hum/skech_esp8266wifi8_motion_temp_hum.ino
@@ -40,7 +40,7 @@ void setup() {
   delay(5000);
 
   WiFi.mode(WIFI_STA);
-  wifiMulti.addAP("Vodafone-83AC", "MfN9rHpHPnsFzMLx");
+  wifiMulti.addAP("YOUR_NETWORK_ID", "YOUR_WIFI_PASSWORD");
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, "Connecting Wifi...");
   Heltec.display->display();
@@ -64,13 +64,7 @@ void loop() {
     Heltec.display->drawString(10, 0, "WiFi not connected!");
     delay(1000);
   } else {
-    //Heltec.display->clear();
-    //Heltec.display->drawString(0,0,"WiFi connected");
-
-    //Heltec.display->drawString(0,10,"IP address: ");
     String broadCast = IpAddress2String(WiFi.localIP());
-    //Heltec.display->drawString(0,20,broadCast);
-    //Heltec.display->display();
     int chk = DHT.read11(DHT11_PIN);
     String statuss = "";
     switch (chk)
@@ -96,7 +90,6 @@ void loop() {
     Heltec.display->drawString(0, 10, "Humidity: " + humidity);
     int sensor = digitalRead(sensorPin);
     String motion = String(sensor,2);
-    //Heltec.display->drawString(0, 20, "IP: " + broadCast);
     Heltec.display->drawString(0, 20, "Motion: " + motion);
     Heltec.display->display();
     
@@ -106,7 +99,7 @@ void loop() {
     WiFiClient client;
 
     HTTPClient http;
-    String url = "http://myhomenikola.pythonanywhere.com/add_data?temp=" + temperature + "&humid=" + humidity + "&ip=" + broadCast + "&motion=" + motion;
+    String url = "http://your_site.pythonanywhere.com/add_data?temp=" + temperature + "&humid=" + humidity + "&ip=" + broadCast + "&motion=" + motion;
 
     if (http.begin(client, url)) {  // HTTP
 
@@ -126,7 +119,5 @@ void loop() {
     }
     delay(1000);
   }
-  // Now it needs to send data to http://myhomenikola.pythonanywhere.com/add_data?temp=10*C&humid=43%&ip=172.20.123.23&motion=11
-  // put your main code here, to run repeatedly:
 
 }
